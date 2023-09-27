39b14b1ab55adf49d2839a54d62259c4842b624d
Ali Yasin Emir <aliyasin.emir16@gmail.com>
2023-09-27 11:13:47 +0300

    I've implemented the popular categories section.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,21 +2,53 @@ import Image from "next/image";
 
 export default function Home() {
 
+  const categories = [
+    {
+      name: 'Fashion',
+      alt: 'fashion category image',
+      img: 'https://images.unsplash.com/photo-1483985988355-763728e1935b'
+    },
+    {
+      name: 'Food',
+      alt: 'food category image',
+      img: 'https://images.unsplash.com/photo-1476224203421-9ac39bcb3327'
+    },
+    {
+      name: 'Coding',
+      alt: 'coding category image',
+      img: 'https://images.unsplash.com/photo-1618477247222-acbdb0e159b3'
+    },
+    {
+      name: 'Style',
+      alt: 'style category image',
+      img: 'https://images.unsplash.com/photo-1525299374597-911581e1bdef'
+    },
+    {
+      name: 'Travel',
+      alt: 'travel category image',
+      img: 'https://images.unsplash.com/photo-1527631746610-bca00a040d60'
+    },
+    {
+      name: 'Culture',
+      alt: 'culture category image',
+      img: 'https://images.unsplash.com/photo-1519181245277-cffeb31da2e3'
+    },
+  ]
 
   return (
-   <div className='h-screen'>
-    <div className="max-container flex flex-col py-36 max-md:py-24 max-md:px-6">
-      <h1 className="text-4xl leading-relaxed font-bold pb-6 max-md:text-3xl">Hey, Reacton dev here!
+   <div className=''>
+    <div className="max-container flex flex-col py-20 max-md:py-12 max-md:px-6">
+      <h1 className="text-4xl leading-relaxed max-md:leading-normal font-bold pb-6 max-md:text-3xl">Hey, Reacton dev here!
       <br />
-          <span className="text-4xl leading-relaxed font-normal max-md:text-2xl">Discover my stories and creative ideas.</span>
+          <span className="text-4xl leading-relaxed max-md:leading-normal font-normal max-md:text-2xl">Discover my stories and creative ideas.</span>
       </h1>
       
       {/* ITEMS */}
       <div className="flex flex-col gap-8">
         {/* ITEM */}
-        <div className="flex bg-slate-400 max-md:bg-transparent max-md:flex-col min-h-[420px] max-md:h-[620px]"> 
+        <div className="flex max-md:bg-transparent max-md:flex-col min-h-[420px] max-md:h-[620px]"> 
         {/* ITEM Image Box */}
-        <div className="relative flex flex-1">
+        <div className="relative flex flex-1 max-md:mb-8">
           <Image src='/main_1.jpg' fill alt="main image" className='object-cover absolute'/>
         </div>
         {/* ITEM Text Box */}
@@ -32,6 +64,30 @@ export default function Home() {
 
       </div>
         {/* ITEMS */}
+
+      {/* POPULAR CATEGORIES */}
+        <section className="flex flex-col py-12 gap-9  max-md:items-center">
+          <h1 className="text-2xl max-md:text-3xl">Popular Categories</h1>
+        {/* CATEGORY BOX */}
+          <div className="flex justify-around max-md:flex-col max-md:gap-6">
+          {/* CATEGORY ITEM */}
+        { categories.map((category) => (
+          <div key={category.name} className="flex gap-3 relative items-center">
+          <Image src={category.img}
+          alt={category.alt}
+          width={32} height={32}
+          className="object-cover rounded-full aspect-square"
+          />
+          <span>{category.name}</span>
+        </div>
+          ))
+        }
+          {/* CATEGORY ITEM */}
+          </div>
+        {/* CATEGORY BOX */}
+        </section>
+
+
     </div>
    </div>
   )
--- src/components/navbar/page.jsx
+++ src/components/navbar/page.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
   const {toggle, mode} = useContext(ThemeContext)
 
   return (
-    <div className="bg-slate-400 w-full p-4">
+    <div className="w-full p-4">
         <div className="max-container flex justify-between max-md:px-2">
         <div className="flex gap-4 max-md:hidden">
             <Link href='https://facebook.com'><Image src='/facebook.svg' alt="facebook icon" width={24} height={24} /></Link>
@@ -44,7 +44,7 @@ const Navbar = () => {
             <Link href='https://www.tiktok.com'><Image className="rounded-full p-1 bg-white" src='/tiktok.svg' alt="tiktok icon" width={24} height={24} /></Link>
             <Link href='https://www.youtube.com'><Image className="rounded-full p-1 bg-white" src='/youtube.svg' alt="youtube icon" width={24} height={24} /></Link>        
         </div>
-            <Link href='/'>Reacton Blog</Link>
+            <Link href='/' className="text-xl max-md:text-lg">Reacton Blog</Link>
             <div className='flex gap-4'>
               <div onClick={toggle} className={`flex bg-slate-500 rounded-full border-2 px-1 border-black relative cursor-pointer items-center justify-between`}>
                 <div className={`flex w-[18px] h-[18px] bg-slate-950 rounded-full absolute ${mode === 'dark' ? 'left-[6px]' : 'right-[6px]'}`}/>
@@ -61,11 +61,11 @@ const Navbar = () => {
             </div>
                 <RxHamburgerMenu onClick={() => setIsSideMenuOpen(!isSideMenuOpen)} className="w-6 h-6 cursor-pointer hidden max-md:block z-20"/>
                   { isSideMenuOpen &&
-                <div className="bg-slate-700 absolute top-0 right-0 w-1/2 h-screen hidden max-md:block bg-opacity-30 backdrop-filter backdrop-blur-lg rounded-l-lg z-10">
+                <div className="absolute top-0 right-0 w-1/2 h-screen hidden max-md:block bg-opacity-30 backdrop-filter backdrop-blur-lg rounded-l-lg z-10">
   
                   <div className="pt-20 flex flex-col gap-4"> 
                 {
-                menu.map((item) => <Link className="items-center w-full p-4 hover:bg-slate-800 text-center hover:bg-opacity-30 hover:backdrop-filter hover:backdrop-blur-lg" href={item.link} key={item.name}>
+                menu.map((item) => <Link className="items-center w-full p-4 hover:bg-gray-600 text-center hover:bg-opacity-30 hover:backdrop-filter hover:backdrop-blur-lg" href={item.link} key={item.name}>
                   {item.name}
                 </Link>
                 )
